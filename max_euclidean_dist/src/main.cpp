92e63ff5bb3e87b2c3ba6e97e4d8e23725a5ce73
Knowles Atchison Jr <code@knowlesatchison.net>
2018-01-07 16:55:29 -0500

    Adding polynomial algo to max euc example

--- max_euclidean_dist/src/main.cpp
+++ max_euclidean_dist/src/main.cpp
@@ -94,6 +94,56 @@ void calculate_all_dist_opt(const Point& start, vector<Movement>& movements, set
   }
 }
 
+//return the bearing degree (we'll subtract 180 to see if we're moving farther away from the origin)
+double bearing(double a1, double a2, double b1, double b2) {
+  static const double TWOPI = 6.2831853071795865;
+  static const double RAD2DEG = 57.2957795130823209;
+  double theta = atan2(b1 - a1, a2 - b2);
+  if (theta < 0.0)
+      theta += TWOPI;
+  return RAD2DEG * theta;
+}
+
+void calculate_all_dist_polynomial_time(const Point& start, vector<Movement>& movements, set<Result, result_cmp>& results){
+  cout << "[polynomial] Starting at: " << start << " with " << movements.size() << " max moves\n";
+  Point tmp(start);
+  int count  = 0;
+  int max_dist = 0;
+  // O(n^2)
+  for(int i = 0; i < movements.size(); i++){
+    Result result;
+    int x_sum = 0;
+    int y_sum = 0;
+    Movement& one = movements.at(i);
+    x_sum += movements.at(i).x;
+    y_sum += movements.at(i).y;
+    result.movements.push_back(movements.at(i));
+    for(int j = 0; j < movements.size(); j++){
+      Movement& two = movements.at(j);
+      if(one == two) continue; //don't evaluate the same one, does not take care of edge case of multiple of the same value
+      double degree = bearing(x_sum, y_sum, (x_sum+two.x), (y_sum+two.y));
+      if ((degree - 180) >= 0){
+        result.movements.push_back(movements.at(j));
+	x_sum += movements.at(j).x;
+	y_sum += movements.at(j).y;
+	Point end(x_sum, y_sum);
+	result.dist = euc_dist(start, end);
+	result.moves = result.movements.size();
+	if(result.dist >= max_dist){
+	  results.insert(result);
+	  max_dist = result.dist;
+	}
+      }
+    }
+  }
+  //cull out any < max distance elements that were added before actual max was discovered
+  set<Result, result_cmp>::iterator it = results.begin();
+  while(it != results.end()){
+    if (it->dist < max_dist) results.erase(it);
+    it++;
+  }
+}
+
 int main(){
   Point start(0,0);
   vector<Movement> movements;
@@ -108,6 +158,9 @@ int main(){
   for(const Result& result : results) cout << result << endl;
   results.clear();
   calculate_all_dist_opt(start, movements, results);
+  for(const Result& result : results) cout << result << endl;
+  results.clear();
+  calculate_all_dist_polynomial_time(start, movements, results);
   for(const Result& result : results) cout << result << endl;
   return 0;
 }
